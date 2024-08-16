d783bb5a532551a65835647ac2f970b284342f04
chxmek <Chawanwits.mek@gmail.com>
2024-08-16 16:35:01 +0700

    Add project files.

---
+++ c-sharp-oop/Program.cs
@@ -0,0 +1,16 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();                      // ปิดการแสดงข้อความเสริมใน console
+        }
+    }
+}
